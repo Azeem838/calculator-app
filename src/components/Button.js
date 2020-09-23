006171e739c2dd7cb40d88c68eeff1de54a9f397
Azeem <azeemahmed101@gmail.com>
2020-09-23 11:47:19 +0200

    reconfigure Button component to be used in buttonPanel and add props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => <div>{name}</div>;
+const Button = ({ name, color, wide }) => (
+  <button
+    style={{ backgroundColor: color, width: wide ? '50%' : '25%' }}
+    type="button"
+  >
+    {name}
+  </button>
+);
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
 };
 
 export default Button;

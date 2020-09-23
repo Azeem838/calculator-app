4d2bc697490032e3933a73969ae79fa6535197d6
Azeem <azeemahmed101@gmail.com>
2020-09-23 09:38:32 +0200

    add app.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ButtonPanel from './ButtonPanel';
+import Button from './Button';
+
+export default function App() {
+  return (
+    <div id="buttonLayout">
+      <Button />
+      <ButtonPanel />
+    </div>
+  );
+}

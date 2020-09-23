6d37323972cbf579abfdb6790f14d60760dda2e4
Azeem <azeemahmed101@gmail.com>
2020-09-23 23:42:45 +0200

    add handleClick and onlClick method in Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, color, wide }) => (
-  <button
-    style={{ backgroundColor: color, width: wide ? '50%' : '25%' }}
-    type="button"
-  >
-    {name}
-  </button>
-);
+export default class Button extends React.Component {
+  handleClick(buttonName) {
+    const { handleClick } = this.props;
+    return handleClick(buttonName);
+  }
+
+  render() {
+    const { name, color, wide } = this.props;
+    return (
+      <button
+        style={{ backgroundColor: color, width: wide ? '50%' : '25%' }}
+        type="button"
+        onClick={this.handleClick}
+      >
+        {name}
+      </button>
+    );
+  }
+}
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   color: PropTypes.string,
   wide: PropTypes.bool,
+  handleClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
   color: 'orange',
   wide: false,
 };
-
-export default Button;

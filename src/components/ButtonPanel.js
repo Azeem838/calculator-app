46a647d90f2068bbf5cd5eafaa3128f4dfbe15f9
Azeem <azeemahmed101@gmail.com>
2020-09-23 23:42:11 +0200

    add handleClick and onClick method in Button.js

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,42 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-export default function ButtonPanel() {
-  return (
-    <div id="buttons">
-      <div id="group1">
-        <Button name="AC" color="#dfdfdf" />
-        <Button name="+/-" color="#dfdfdf" />
-        <Button name="%" color="#dfdfdf" />
-        <Button name="÷" />
-      </div>
+export default class ButtonPanel extends React.Component {
+  handleClick(buttonName) {
+    const { handleClick } = this.props;
+    return handleClick(buttonName);
+  }
 
-      <div id="group2">
-        <Button name="7" color="#dfdfdf" />
-        <Button name="8" color="#dfdfdf" />
-        <Button name="9" color="#dfdfdf" />
-        <Button name="X" />
-      </div>
+  render() {
+    return (
+      <div id="buttons">
+        <div id="group1">
+          <Button onClick={this.handleClick} name="AC" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="+/-" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="%" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="÷" />
+        </div>
 
-      <div id="group3">
-        <Button name="4" color="#dfdfdf" />
-        <Button name="5" color="#dfdfdf" />
-        <Button name="6" color="#dfdfdf" />
-        <Button name="-" />
-      </div>
+        <div id="group2">
+          <Button onClick={this.handleClick} name="7" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="8" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="9" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="X" />
+        </div>
 
-      <div id="group4">
-        <Button name="1" color="#dfdfdf" />
-        <Button name="2" color="#dfdfdf" />
-        <Button name="3" color="#dfdfdf" />
-        <Button name="+" />
-      </div>
+        <div id="group3">
+          <Button onClick={this.handleClick} name="4" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="5" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="6" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="-" />
+        </div>
 
-      <div id="group5">
-        <Button name="0" wide color="#dfdfdf" />
-        <Button name="." color="#dfdfdf" />
-        <Button name="=" />
+        <div id="group4">
+          <Button onClick={this.handleClick} name="1" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="2" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="3" color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="+" />
+        </div>
+
+        <div id="group5">
+          <Button onClick={this.handleClick} name="0" wide color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="." color="#dfdfdf" />
+          <Button onClick={this.handleClick} name="=" />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+ButtonPanel.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};

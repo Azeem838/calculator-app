5b37b670aac93c0e838a1232550a686e6572a342
Azeem <azeemahmed101@gmail.com>
2020-09-23 23:41:36 +0200

    update App.js toinclude stateand handleClick methods

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-export default function App() {
-  return (
-    <div id="calculator">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState((prevState) => {
+      const data = calculate(prevState, buttonName);
+      return {
+        total: data.total,
+        next: data.next,
+        operation: data.operation,
+      };
+    });
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <div id="calculator">
+        <Display result={total} />
+        <ButtonPanel onClick={this.handleClick} />
+      </div>
+    );
+  }
 }

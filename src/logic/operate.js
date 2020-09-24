211046fa69c9ba121f1ecafa9659d3409bb2c8f9
Azeem <azeemahmed101@gmail.com>
2020-09-24 12:58:36 +0200

    update operate method

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,31 +2,31 @@ import Big from 'big.js';
 
 export default function operate(numOne, numTwo, operation) {
   let total;
-  const numOneBig = new Big(numOne);
-  const numTwoBig = new Big(numTwo);
+  const numOneBig = new Big(parseFloat(numOne));
+  const numTwoBig = new Big(parseFloat(numTwo));
   switch (operation) {
     case '+':
-      total = numOneBig + numTwoBig;
+      total = numOneBig.plus(numTwoBig).toString();
       break;
 
     case '-':
-      total = numOneBig - numTwoBig;
+      total = numOneBig.minus(numTwoBig).toString();
       break;
 
     case 'X':
-      total = numOneBig * numTwoBig;
+      total = numOneBig.times(numTwoBig).toString();
       break;
 
     case '÷':
-      total = numOneBig / numTwoBig;
-      break;
-
-    case '%':
-      total = numOneBig * (numTwoBig / 100);
+      try {
+        total = numOneBig.div(numTwoBig).toString();
+      } catch (error) {
+        total = 'NaN';
+      }
       break;
 
     default:
-      total = 0;
+      total = '0';
       break;
   }
   return total;

c6c04db6a1cb78a548c106ef83a89eaae996671c
Azeem <azeemahmed101@gmail.com>
2020-09-24 08:54:13 +0200

    merge develop ButtonPanel.js

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,65 +5,36 @@ export default function ButtonPanel() {
   return (
     <div id="buttons">
       <div id="group1">
-<<<<<<< HEAD
         <Button name="AC" color="#dfdfdf" />
         <Button name="+/-" color="#dfdfdf" />
         <Button name="%" color="#dfdfdf" />
-=======
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
->>>>>>> develop
         <Button name="÷" />
       </div>
 
       <div id="group2">
-<<<<<<< HEAD
         <Button name="7" color="#dfdfdf" />
         <Button name="8" color="#dfdfdf" />
         <Button name="9" color="#dfdfdf" />
-=======
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
->>>>>>> develop
         <Button name="X" />
       </div>
 
       <div id="group3">
-<<<<<<< HEAD
         <Button name="4" color="#dfdfdf" />
         <Button name="5" color="#dfdfdf" />
         <Button name="6" color="#dfdfdf" />
-=======
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
->>>>>>> develop
         <Button name="-" />
       </div>
 
       <div id="group4">
-<<<<<<< HEAD
         <Button name="1" color="#dfdfdf" />
         <Button name="2" color="#dfdfdf" />
         <Button name="3" color="#dfdfdf" />
-=======
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
->>>>>>> develop
         <Button name="+" />
       </div>
 
       <div id="group5">
-<<<<<<< HEAD
         <Button name="0" wide color="#dfdfdf" />
         <Button name="." color="#dfdfdf" />
-=======
-        <Button name="0" />
-        <Button name="." />
->>>>>>> develop
         <Button name="=" />
       </div>
     </div>

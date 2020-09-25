2559d9bccc8b5532c8561e1cef9533989845daa4
Azeem <azeemahmed101@gmail.com>
2020-09-25 20:18:41 +0200

    fix overflow of display

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,24 +2,25 @@ import Big from 'big.js';
 
 export default function operate(numOne, numTwo, operation) {
   let total;
+  Big.PE = 10;
   const numOneBig = new Big(parseFloat(numOne));
   const numTwoBig = new Big(parseFloat(numTwo));
   switch (operation) {
     case '+':
-      total = numOneBig.plus(numTwoBig).toString();
+      total = numOneBig.plus(numTwoBig).toPrecision(5).toString();
       break;
 
     case '-':
-      total = numOneBig.minus(numTwoBig).toString();
+      total = numOneBig.minus(numTwoBig).toPrecision(5).toString();
       break;
 
     case 'X':
-      total = numOneBig.times(numTwoBig).toString();
+      total = numOneBig.times(numTwoBig).toPrecision(5).toString();
       break;
 
     case '÷':
       try {
-        total = numOneBig.div(numTwoBig).toString();
+        total = numOneBig.div(numTwoBig).toPrecision(5).toString();
       } catch (error) {
         total = 'NaN';
       }
